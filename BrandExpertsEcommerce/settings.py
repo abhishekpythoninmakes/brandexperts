3e17549d375f2c9f4f31c3453286e14927949711
abhishekpythoninmakes <abhishekar3690@gmail.com>
2025-02-11 10:40:51 +0530

    settings updated

--- BrandExpertsEcommerce/settings.py
+++ BrandExpertsEcommerce/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-t7*2el()8a*qrz%!p#quy8m-%6rlgb(7m@#xk*!(tk+m95#dk9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'dash.brandexperts.ae'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
