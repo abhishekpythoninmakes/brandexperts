9804b1319f626c58bf8d8cd49d6bc1d1f0de6f14
abhishekpythoninmakes <abhishekar3690@gmail.com>
2025-02-13 20:52:07 +0530

    database mysql connected

--- BrandExpertsEcommerce/settings.py
+++ BrandExpertsEcommerce/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'BrandExpertsEcommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MYBE',
+        'USER': 'admin',
+        'PASSWORD': 'database1brandexperts',
+        'HOST':'databasebe.cz6qg420mik4.ap-south-1.rds.amazonaws.com',
+        'PORT':3306
     }
 }
 
